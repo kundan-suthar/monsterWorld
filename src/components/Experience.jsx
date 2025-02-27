9d7f4f726f18895e81cbbeaead54a5f32d145580
Kundan <kundanss22@gmail.com>
2025-02-27 21:21:35 +0530

    added model and fonts also added MonsterStage component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,66 @@
-import { Environment, OrbitControls, useTexture } from "@react-three/drei";
-import * as THREE from 'three'
+import {
+  Environment,
+  MeshPortalMaterial,
+  OrbitControls,
+  RoundedBox,
+  useTexture,
+} from "@react-three/drei";
+import * as THREE from "three";
+import { Fish } from "./Fish";
+import { Dragon_Evolved } from "./Dragon_Evolved";
+import { Cactoro } from "./Cactoro";
 
 export const Experience = () => {
-  const map = useTexture('textures/anime_art_style_a_water_based_pokemon_like_environ.jpg')
   return (
     <>
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       <OrbitControls />
-      <mesh>
-        <sphereGeometry args={[5, 64, 64]} />
-        <meshStandardMaterial map={map} side={THREE.BackSide} />
-      </mesh>
+      <MonsterStage
+        texture={
+          "textures/anime_art_style_a_water_based_pokemon_like_environ.jpg"
+        }
+      >
+        <Fish scale={0.6} position-y={-1} />
+      </MonsterStage>
+      <MonsterStage
+        texture={
+          "textures/anime_art_style_cactus_forest.jpg"
+        }
+        position-x={-2.5}
+        rotation-y={Math.PI / 8}
+      >
+        <Cactoro scale={0.45} position-y={-1} />
+      </MonsterStage>
+      <MonsterStage
+        texture={
+          "textures/anime_art_style_lava_world.jpg"
+        }
+        position-x={2.5}
+        rotation-y={-Math.PI / 8}
+      >
+        <Dragon_Evolved scale={0.5} position-y={-1} />
+      </MonsterStage>
     </>
+  );
+};
+
+const MonsterStage = ({ children, texture, ...props }) => {
+  const map = useTexture(texture);
+  return (
+    <group {...props}>
+      <RoundedBox args={[2, 3, 0.1]}>
+        <MeshPortalMaterial side={THREE.DoubleSide}>
+          <ambientLight intensity={1} />
+          <Environment preset="sunset" />
+          {/* <Fish scale={0.6} position-y={-1} /> */}
+          {children}
+          <mesh>
+            <sphereGeometry args={[5, 64, 64]} />
+            <meshStandardMaterial map={map} side={THREE.BackSide} />
+          </mesh>
+        </MeshPortalMaterial>
+      </RoundedBox>
+    </group>
   );
 };
