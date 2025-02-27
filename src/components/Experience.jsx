b6a2b9c54e3797296098506e4ec713190e061d6a
Kundan <kundanss22@gmail.com>
2025-02-27 22:11:57 +0530

    added maath package and added smooth transition to portal

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,5 @@
 import {
+  CameraControls,
   Environment,
   MeshPortalMaterial,
   OrbitControls,
@@ -7,45 +8,77 @@ import {
   useTexture,
 } from "@react-three/drei";
 import * as THREE from "three";
+import { easing } from "maath";
 import { Fish } from "./Fish";
 import { Dragon_Evolved } from "./Dragon_Evolved";
 import { Cactoro } from "./Cactoro";
+import { useEffect, useRef, useState } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
 
 export const Experience = () => {
+  const [active, setActive] = useState(null);
+  const controlRef = useRef();
+  const scene = useThree((state) => state.scene)
+  useEffect(() => {
+    if (active) {
+      const targetPosition = new THREE.Vector3();
+      scene.getObjectByName(active).getWorldPosition(targetPosition);
+      controlRef.current.setLookAt(
+        0,
+        0,
+        5,
+        targetPosition.x,
+        targetPosition.y,
+        targetPosition.z,
+        true,
+      )
+    } else {
+      controlRef.current.setLookAt(
+        0,
+        0,
+        10,
+        0,
+        0,
+        0,
+        true,
+      )
+    }
+  }, [active]);
   return (
     <>
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
-      <OrbitControls />
+      <CameraControls ref={controlRef} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 6} />
       <MonsterStage
         texture={
           "textures/anime_art_style_a_water_based_pokemon_like_environ.jpg"
         }
         name="Fish"
         color={"#006b96"}
-
+        active={active}
+        setActive={setActive}
       >
         <Fish scale={0.6} position-y={-1} />
       </MonsterStage>
       <MonsterStage
-        texture={
-          "textures/anime_art_style_cactus_forest.jpg"
-        }
+        texture={"textures/anime_art_style_cactus_forest.jpg"}
         name="Cactoro"
         color={"#577a28"}
         position-x={-2.5}
         rotation-y={Math.PI / 8}
+        active={active}
+        setActive={setActive}
       >
         <Cactoro scale={0.45} position-y={-1} />
       </MonsterStage>
       <MonsterStage
-        texture={
-          "textures/anime_art_style_lava_world.jpg"
-        }
+        texture={"textures/anime_art_style_lava_world.jpg"}
         name="Dragon"
         color={"#df8d52"}
         position-x={2.5}
         rotation-y={-Math.PI / 8}
+        active={active}
+        setActive={setActive}
       >
         <Dragon_Evolved scale={0.5} position-y={-1} />
       </MonsterStage>
@@ -53,8 +86,21 @@ export const Experience = () => {
   );
 };
 
-const MonsterStage = ({ children, texture, name, color, ...props }) => {
+const MonsterStage = ({
+  children,
+  texture,
+  name,
+  color,
+  active,
+  setActive,
+  ...props
+}) => {
   const map = useTexture(texture);
+  const portalMaterial = useRef();
+  useFrame((_state, delta) => {
+    const worldOpen = active === name;
+    easing.damp(portalMaterial.current, "blend", worldOpen ? 1 : 0, 0.2, delta);
+  });
   return (
     <group {...props}>
       <Text
@@ -65,8 +111,12 @@ const MonsterStage = ({ children, texture, name, color, ...props }) => {
         {name}
         <meshBasicMaterial color={color} toneMapped={false} />
       </Text>
-      <RoundedBox args={[2, 3, 0.1]}>
-        <MeshPortalMaterial side={THREE.DoubleSide}>
+      <RoundedBox
+        name={name}
+        args={[2, 3, 0.1]}
+        onDoubleClick={() => setActive(active === name ? null : name)}
+      >
+        <MeshPortalMaterial side={THREE.DoubleSide} ref={portalMaterial}>
           <ambientLight intensity={1} />
           <Environment preset="sunset" />
           {/* <Fish scale={0.6} position-y={-1} /> */}
